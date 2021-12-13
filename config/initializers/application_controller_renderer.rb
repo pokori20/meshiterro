025833c491e1e160698782f536b167747fd6975d
pokori20 <93557699+pokori20@users.noreply.github.com>
2021-12-13 10:27:23 +0000

    [finish]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '09a796394b5db5aa349fcb754e7a34f4a28c7a94fc16267b644d6ce1bcde8c697e0c3313b2e7ad2851f55efe2b1d5c6da9cccfe7f44ac4834c82ceebfbbe04b3'
